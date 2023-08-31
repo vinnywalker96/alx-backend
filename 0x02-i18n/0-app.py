be07be4e6f9e4868b49f5b29a568227aa83fd038
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-08-31 12:27:25 +0000

    hello world

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
