566dfb1a595e5b83e2af7ed818b6137f5cddc0bf
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-02 07:31:55 +0000

    Babel config

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,16 +4,18 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel()
 
-class Config:
+
+class Config(object):
     """Config Class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
-babel.init(app)
+
+babel = Bable(app)
 
 
 @app.route('/')
