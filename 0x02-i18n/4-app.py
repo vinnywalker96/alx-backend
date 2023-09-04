9178852a33ba04ec25e5f9436ca68ef2a82d9ad6
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-04 07:31:44 +0000

    force translation

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,32 +1,38 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ FLask APP"""
-from flask import Flask, render_template
-from flask import request
-from flask_babel import Babel, request, _
+from flask import Flask, render_template, request
+from flask_babel import Babel, _, get_locale
 
 app = Flask(__name__)
 
 
-
 class Config(object):
-    """Flask Config"""
+    """Config Class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
+
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
-    user_locale = request.args.get('locale')
-    return user_locale
-
-
+    """Request languages"""
+    if request.args.get('locale') in app.config['LANGUAGES']:
+        return request.args.get('locale')
+    return 
 
 @app.route('/')
 def index():
+    """
+    return renders
+    html page
+    """
     return render_template('4-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
