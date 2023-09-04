648950df0cfb45e3974208b44705e3ea68235804
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-04 20:42:54 +0000

    Simple helper

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Simple Pagination"""
+import csv
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return Page range"""
+        assert (
+            isinstance(page, int) and page > 0
+        ), "Page must be a positive integer"
+        assert (
+            isinstance(page_size, int) and page_size > 0
+        ), "Page size must be a positive integer"
+
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+        if start_index is not None and end_index is not None:
+            return data[start_index:end_index]
+
+        return []
