19e7749ff068105e631970693b8b93c4819f9c00
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-06 15:27:20 +0000

    Pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -50,8 +50,8 @@ class Server:
         """
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
-        next_page = page - 1 if page < total_pages else None
-        prev_page = page + 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
         page_data = {
                 'page_size': page_size,
                 'page': page,
