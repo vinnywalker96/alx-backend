a5952870d727b0f32975b31f2738b160e129822d
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-06 05:38:14 +0000

    Get current time

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,47 +1,63 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ FLask APP"""
-from flask import Flask, render_template
-from flask import request
-from flask_babel import Babel, request, _
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
 
-app = Flask(__name__)
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config(object):
-    """Flask Config"""
+    """Config Class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
-@babel.localeselector
-def get_locale():
-    user_locale = request.args.get('locale')
-    return user_locale
 
 def get_user():
-    users = {
-             1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-             2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-             3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-             4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-    }
-    login = request.args.get('login_as')
-    if login in  users:
-        return users[login]
+    """Validate User"""
+    id = request.args.get('login_in_as', None)
+    if id is not None and int(id) in users.keys():
+        return user.get(int(id))
     return None
+
+
+
+@babel.localeselector
+def get_locale():
+    """Request languages"""
+    user_req = request.args.get('locale')
+    if user_req in app.config['LANGUAGES']:
+        return user_req
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 
 
 @app.before_request
 def before_request():
-    return
+    """Get global user"""
+    user = get_user()
+    g.user = user
 
 @app.route('/')
 def index():
+    """
+    return renders
+    html page
+    """
     return render_template('5-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
