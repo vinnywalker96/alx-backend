1b423e3e57751edb7e8c37c5c6375590ff63ffac
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-09-11 14:52:42 +0200

    LIFO Cache

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
 """LIFO Caching"""
+BaseCaching = __import__('base_caching').BaseCaching
 
 
+class LIFOCache(BaseCaching):
+    """LIFOCache inherit from BaseCaching"""
+    def __init__(self):
+        """Initiation"""
+        super().__init__()
+
+    def put(self, key: str, item: str) -> None:
+        """Updates Dic on base class"""
+        if key is None or item is None:
+            return None
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last = list(self.cache_data.keys())[-1]
+            self.cache_data.pop(last)
+            print(f"Discard: {last}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get value"""
+        if key is None or self.cache_data[key] is None:
+            return None
+        return self.cache_data[key]
