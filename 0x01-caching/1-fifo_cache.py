03567e7530d5b483c5a939d4eb89c851f674feaa
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-23 08:21:03 +0200

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,29 +1,49 @@
 #!/usr/bin/python3
-"""FIFO Caching"""
+""" 1. FIFO caching
+"""
 
-BaseCaching = __import__('base_caching').BaseCaching
+from collections import deque
+
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """ class FIFOCache that inherits from BaseCaching and is a caching system
     """
-    FIFOCache inherits BaseCaching
-    """
+
     def __init__(self):
-        """Initializing """
+        """ Init
+        """
         super().__init__()
+        self.queue = deque()
 
     def put(self, key, item):
-        """Add item to basecache"""
-        if key is None or item is None:
-            return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            first = next(iter(self.cache_data))
-            del self.cache_data[first]
-            print(f"DISCARD: {first}")
-        self.cache_data[key] = item
+        """ Must assign to the dictionary self.cache_data the item value for
+        the key key.
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif self.is_full():
+                self.evict()
+            self.queue.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Gets value"""
-        if key is None or self.cache_data[key] is None:
-            return None
-        return self.cache_data[key]
+        """ Must return the value in self.cache_data linked to key.
+        """
+        return self.cache_data.get(key, None)
+
+    def is_full(self):
+        """ If the number of items in self.cache_data is higher that
+        BaseCaching.MAX_ITEMS
+        """
+        return len(self.cache_data) >= self.MAX_ITEMS
+
+    def evict(self):
+        """ you must print DISCARD: with the key discarded and following by a
+        new line
+        """
+        popped = self.queue.popleft()
+        del self.cache_data[popped]
+        print("DISCARD: " + str(popped))
