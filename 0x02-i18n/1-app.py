a25d6c334d071ed76d541ab4afd7b1bc5a5aeb05
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-23 08:43:11 +0200

    Advanced Tasks

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
-""" FLask APP"""
-from flask import Flask, render_template
+
+"""
+1. Basic Flask app
+"""
+
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
-class Config(object):
-    """Config Class"""
+class Config:
+    """
+    Config class.
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
 
-babel = Babel(app)
 
-
-@app.route('/')
-def index():
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello():
     """
-    return renders
-    html page
+    hello.
     """
     return render_template('1-index.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="5000")
