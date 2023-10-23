7e540ca63e7e2d65c349414709febb0452cef473
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-10-23 09:04:24 +0200

    Advanced Tasks

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-""" FLask APP"""
-from flask import Flask, render_template, request, g
+"""Flask App"""
+from flask import Flask, g, render_template, request
 from flask_babel import Babel
 
+app = Flask(__name__)
+babel = Babel(app)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -13,51 +15,44 @@ users = {
 
 
 class Config(object):
-    """Config Class"""
-    LANGUAGES = ["en", "fr"]
+    """simple configuration"""
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
 
 
 def get_user():
-    """Validate User"""
-    id = request.args.get('login_in_as', None)
-    if id is not None and int(id) in users.keys():
-        return user.get(int(id))
-    return None
-
-
-
-@babel.localeselector
-def get_locale():
-    """Request languages"""
-    user_req = request.args.get('locale')
-    if user_req in app.config['LANGUAGES']:
-        return user_req
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
+    """get user from header"""
+    id = request.args.get('login_as')
+    try:
+        return users.get(int(id))
+    except Exception:
+        return None
 
 
 @app.before_request
 def before_request():
-    """Get global user"""
-    user = get_user()
-    g.user = user
+    """Before request used to stash user"""
+    g.user = get_user()
 
-@app.route('/')
+
+@babel.localeselector
+def get_locale():
+    """locale selector determining lang use for template"""
+    loc = request.args.get('locale')
+    if loc and loc in app.config['LANGUAGES']:
+        return loc
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/", methods=['GET'])
 def index():
-    """
-    return renders
-    html page
-    """
-    return render_template('5-index.html')
+    """index rotue"""
+    return render_template("6-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port="5000")
